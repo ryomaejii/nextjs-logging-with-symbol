9b1a9d5fc81d45c64daf501732b2d69960cac530
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2024-09-28 11:36:56 +0900

    feat: Show Button and ButtonWithLog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
+'use client';
+
+import { ButtonWithLog } from '@/app/components/ButtonWithLog';
 import { Button } from '@/app/components/Buttton';
+import { onClickWithLog } from '@/app/utils/clickLog';
+import { MouseEventHandler, useState } from 'react';
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+  const onClickButton: MouseEventHandler<HTMLButtonElement> = () => {
+    setCount(count + 1);
+  };
+
+  const onClickButtonWithLog = onClickWithLog(onClickButton, () => {
+    console.log('Button clicked');
+  });
+
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-4">
+      <p className="text-2xl font-bold">Count: {count}</p>
+      <div className="flex gap-4">
+        <Button onClick={onClickButton}>Normal Button</Button>
+        {/* エラーになる
+        <ButtonWithLog onClick={onClickButton}>Button with Log</ButtonWithLog> */}
+        <ButtonWithLog onClick={onClickButtonWithLog}>
+          Button with Log
+        </ButtonWithLog>
+      </div>
     </div>
   );
 }
